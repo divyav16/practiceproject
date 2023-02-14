7448b6ea42d82972eb1883b39b52f7747d5a9299
divya vandra <divyavandra@gmail.com>
2023-02-14 22:04:08 +0000

    first version

---
+++ javatopics.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class javatopics {
+    //i have changed class name from java to java topics
+    /*there are so many topics to cover.
+    then after automation will start
+     */
+    public static void main(String[] args) {
+        System.out.println("Automation testing is fast in execution");
+        System.out.println("Automated testing is more reliable");
+        System.out.println("It is also very powerful");
+        System.out.println("It is less time consuming");
+        System.out.println("It helps to increase the test coverage");
+
+    }
+}
